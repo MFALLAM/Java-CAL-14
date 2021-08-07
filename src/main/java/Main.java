ad80b6fe656add9749b6d6690a7a7f1cf2130f9f
MFALLAM <mohammed.fathi.allam@gmail.com>
2021-08-08 00:51:27 +0300

    Homework

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        
+
+        ArrayList<Integer> numbersList = new ArrayList<>(Arrays.asList(1,3,7,5,4,3,4,1,2,9));
+
+        ArrayList<Integer> hasOneOrFour = new ArrayList<>(Arrays.asList(5,3));
+
+        System.out.println(Play.getNewArray(numbersList, 4));
+
+        System.out.println(Play.checkArray(hasOneOrFour));
+
     }
 }
---
+++ src/test/java/TestPlay.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class TestPlay {
+
+    private static Stream<Arguments> streamDataNewArray() {
+        return Stream.of(
+                Arguments.of(new ArrayList<>(Arrays.asList(1, 2, 4, 4, 2, 3, 4, 1, 7)), new ArrayList<>(Arrays.asList(1, 7))),
+                Arguments.of(new ArrayList<>(Arrays.asList(1, 2, 4, 2, 3, 1, 7)), new ArrayList<>(Arrays.asList(2, 3, 1, 7))),
+                Arguments.of(new ArrayList<>(Arrays.asList(1, 2, 4, 2, 3, 1, 4)), new ArrayList<>(Arrays.asList())),
+                Arguments.of(new ArrayList<>(Arrays.asList(1, 2, 3)), new ArrayList<>(Arrays.asList()))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("streamDataNewArray")
+    public void massTestNewArray(ArrayList<Integer> arg, ArrayList<Integer> expectedArray) {
+        ArrayList<Integer> resultArray = Play.getNewArray(arg, 4);
+        Assertions.assertArrayEquals(expectedArray.toArray(), resultArray.toArray());
+    }
+
+
+    private static Stream<Arguments> streamDataOfChecks() {
+        return Stream.of(
+                Arguments.of(new ArrayList<>(Arrays.asList(1, 2, 4, 4, 2, 3, 4, 1, 7)), true),
+                Arguments.of(new ArrayList<>(Arrays.asList(0, 2, 9, 2, 3, 5, 7)), false)
+        );
+    }
+
+    private static Stream<Arguments> streamData() {
+        List<Arguments> list = new ArrayList<>();
+        list.add(Arguments.arguments(new ArrayList<>(Arrays.asList(1, 3))));
+
+        return list.stream();
+    }
+
+    @ParameterizedTest
+    @MethodSource("streamDataOfChecks")
+    public void massTestcheckArray(ArrayList<Integer> arg, boolean expectedArray) {
+        boolean resultArray = Play.checkArray(arg);
+        Assertions.assertEquals(expectedArray, resultArray);
+    }
+
+}
